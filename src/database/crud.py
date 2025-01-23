35d4577257010769b52e64dc3c9f2fc0d528d6d6
Anabel Benítez <anabelbenitezglez@gmail.com>
2025-01-23 12:44:03 -0500

    Add create_if_not_exists and upsert_record functions for improved CRUD operations

--- src/database/crud.py
+++ src/database/crud.py
@@ -15,26 +15,47 @@ def create_record(session, model, **kwargs):
     return obj
 
 
+def create_if_not_exists(session, model, filters, **kwargs):
+    obj = session.query(model).filter_by(**filters).one_or_none()
+
+    if obj is None:
+        obj = model(**{**filters, **kwargs})
+        session.add(obj)
+        session.commit()
+        session.refresh(obj)
+    return obj
+
+
+# UPSERT
+def upsert_record(session, model, filters, **update_data):
+    obj = session.query(model).filter_by(**filters).one_or_none()
+
+    if obj:
+        # Update the existing record
+        for key, value in update_data.items():
+            setattr(obj, key, value)
+        session.commit()
+    else:
+        # Create a new record
+        obj = model(**{**filters, **update_data})
+        session.add(obj)
+        session.commit()
+        session.refresh(obj)
+    return obj
+
+
 # READ
 def get_record_by_id(session, model, record_id):
     return session.query(model).get(record_id)
 
 
 def single_or_default(session, model, **filters):
-    """
-    Retrieves a single record based on the provided filters.
-    Raises an exception if multiple records match.
-    """
     query = session.query(model).filter_by(**filters)
     result = query.one_or_none()  # Returns one record or None if no match.
     return result
 
 
 def first_or_default(session, model, **filters):
-    """
-    Retrieves the first record based on the provided filters.
-    Returns None if no record matches.
-    """
     query = session.query(model).filter_by(**filters)
     return query.first()
 
